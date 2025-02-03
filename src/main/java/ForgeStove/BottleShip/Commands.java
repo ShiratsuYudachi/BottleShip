79652e4ece02e0e655e43863880a752917db8c97
ForgeStove <2071948808@qq.com>
2025-02-03 17:04:48 +0800

    fix(ui): 修改命令中使用船的标识方式

--- src/main/java/ForgeStove/BottleShip/Commands.java
+++ src/main/java/ForgeStove/BottleShip/Commands.java
@@ -7,12 +7,16 @@ import org.valkyrienskies.core.api.ships.Ship;
 import static java.lang.System.err;
 public class Commands {
 	public static void teleport(
-			@NotNull String playerName, @NotNull Ship ship,
-			MinecraftServer server, long x, long y, long z
+			@NotNull String playerName,
+			@NotNull Ship ship,
+			MinecraftServer server,
+			long x,
+			long y,
+			long z
 	) {
 		executeCommand(
 				String.format(
-						"execute at %s run vmod teleport %s ~%s ~%s ~%s (0 0 0)", playerName, ship.getSlug(),
+						"execute at %s run vmod teleport @v[id=%s] ~%s ~%s ~%s (0 0 0)", playerName, ship.getId(),
 						x,
 						y,
 						z
@@ -27,6 +31,6 @@ public class Commands {
 		}
 	}
 	public static void setStatic(@NotNull Ship ship, MinecraftServer server, boolean isStatic) {
-		executeCommand(String.format("vs set-static %s %s", ship.getSlug(), isStatic), server);
+		executeCommand(String.format("vs set-static @v[id=%s] %s", ship.getId(), isStatic), server);
 	}
 }
